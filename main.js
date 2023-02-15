dd904c47e497304f221ab46cad5e3ffd9810196c
JaedonM <114765003+JaedonM@users.noreply.github.com>
2023-02-15 17:45:40 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+function setup(){
+canvas = createCanvas(640, 480)
+canvas.position(30,270)
+
+video = createCapture(VIDEO)
+video.hide()
+color = ""
+}
+
+function draw(){
+
+    image(video, 0,0,640,480)
+
+    tint(color)
+
+}
+
+function apply(){
+    color = document.getElementById("color").value;
+}
+
+
+function take_snapshot(){
+    save("image.png")
+}
